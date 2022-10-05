6550fcdb64ef1d7ec2a873bbcdb29c3beb47ac23
Fiona Eckert <fionaeckert@fionas-air.lan>
2022-10-05 14:46:52 -0500

    adding work related to ptwo-3, ptwo-4, ptwo-5

--- index.js
+++ index.js
@@ -5,21 +5,21 @@ app.set('view engine', 'ejs')
 const { users } = require('./models')
 const bcrypt = require('bcrypt');
 const saltRounds = 8;
-const myPlaintextPassword = 's0/\/\P4$$w0rD';
-const someOtherPlaintextPassword = 'not_bacon';
-let error = null
+let error = ''
 
 app.use(bodyParser.urlencoded({ extended: false }))
+
 
 app.get('/', (req, res)=> {
     res.render("signUp",{
         error : error
     })
+    error = ''
 })
 
 
 app.post('/createuser', async (req, res) => {
-    error = null
+    error = ''
     const user = await users.findOne({
         where: {
             username : req.body.username
@@ -28,40 +28,50 @@ app.post('/createuser', async (req, res) => {
     })
     
     var regex = /^[A-Za-z]+$/;
-
-    if(regex.test(req.body.firstname)){
+    var userregex = /^[0-9A-Za-z]+$/;
+    var pwregex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{6,20}$/
+    console.log(regex.test(req.body.firstname))
+    
+    if(regex.test(req.body.firstname) == false){
         // alert('Please enter a valid first name.')
         error = 'Please enter a valid first name.'
     }
-    else if(typeof(req.body.lastname)!='string'){
+    else if(regex.test(req.body.lastname) == false){
         // alert('Please enter a valid last name.')
         error = 'Please enter a valid last name.'
     }
-    else if(typeof(req.body.username)!='string'){
+    else if(userregex.test(req.body.username) == false){
         // alert('Please enter a valid username.')
-        error = true
+        error = 'Please enter a valid username.'
     }
-    else if(typeof(req.body.password)!='string'){
+    else if(pwregex.test(req.body.password) == false){
         // alert('Please enter a valid password.')
-        error = true
+        error = "Please enter a valid password. Your password must have at least one uppercase letter, at least one lowercase letter, at least one special character, and at least one number."
     }
-    else if(Object.keys(req.body.password).length < 6 || Object.keys(req.body.password).length > 20){
+    else if(req.body.password.length < 6 || req.body.password.length > 20){
         // alert('Please enter a password between 6-20 characters.')
-        error = true
+        error = 'Please enter a password between 6-20 characters.'
+    }
+    else if(req.body.password != req.body.confirmpassword) {
+        error = "Passwords do not match"
     }
     else {
-        error = null
+        error = ''
     }
     
-    if(user == null && error == null) {
-        await users.create({
+    if(user == null && error == '') {
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            bcrypt.hash(req.body.password, salt, async function(err, hash) {
+        users.create({
         firstName: req.body.firstname,
         lastName: req.body.lastname,
         username: req.body.username,
-        password: req.body.password
+        password: hash
         })
+  })
+})
     }
-    else {
+    else if(error == '') {
         error = 'username already exists'
     }
     
