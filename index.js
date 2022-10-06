b81bdc0d0814cd476bdfb9cd7f7319072c2d0fca
Fiona Eckert <fionaeckert@Fionas-MacBook-Air.local>
2022-10-06 13:42:42 -0500

    removing hard setting of port so that Heroku can dynamically set port

--- index.js
+++ index.js
@@ -140,4 +140,6 @@ app.post('/createuser', async (req, res) => {
     res.redirect('/registration')
 })
 
-app.listen(3000, console.log('Server running on port 3000'))
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
