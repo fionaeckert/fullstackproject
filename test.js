858251d5d532681c070377df1a599bc459f16e40
Fiona Eckert <fionaeckert@Fionas-MacBook-Air.local>
2022-11-10 11:37:32 -0600

    undoing changes

---
+++ test.js
@@ -0,0 +1,6 @@
+
+describe('Check type', () => {
+    it('we expect variable key to be a string | expect style', () => {
+        expect(saltRounds).to.be.a('number')
+        console.log('1st test')
+    })})
